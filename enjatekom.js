e527ee6b5bb2d76cf177f3e180c5baf77e4519d2
magyar-matyas <magyar.matyas@students.jedlik.eu>
2024-01-13 14:56:54 +0100

    játék folytatása Fixes #8

--- enjatekom.js
+++ enjatekom.js
@@ -1,75 +1,84 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const nits = document.querySelector('.nits');
-    const grid = document.querySelector('.grid');
-    const alert = document.querySelector('#alert');
+var jatekos = document.getElementById("jatekos");
+        var jatekPalya = document.getElementById("jatekPalya");
+        var pontszam = 0;
+        var jatekVege = false;
 
-    let position = 0;
-    let gravity = 0.9;
-    let isJumping = false;
-    let isGameOver = false;
-    let score = 0;
+        function kepGeneral(type) {
+            var zoldsegek = document.createElement("div");
+            zoldsegek.classList.add("item");
+            zoldsegek.classList.add(type);
 
-    function JatekosMozgatas(event){
-        if(event.keyCode == 37){ //bal
-            jatekosPozicio -= 200;
-            if(jatekosPozicio >= 0){
-                jatekos.style.left = jatekosPozicio + 'px';
-                console.log(jatekosPozicio)
-            }
-            else jatekosPozicio += 200;
+            var random = Math.floor(Math.random() * (jatekPalya.clientHeight - 50));
+            zoldsegek.style.top = random + "px";
+            zoldsegek.style.left = jatekPalya.clientWidth + "px";
+
+            jatekPalya.appendChild(zoldsegek);
+
+            return zoldsegek;
         }
-        if(event.keyCode == 39){ //jobb
-            jatekosPozicio += 200;
-            if(jatekosPozicio < 600){
-                jatekos.style.left = jatekosPozicio + 'px';
-                console.log(jatekosPozicio)
-            }
-            else jatekosPozicio -= 200;
-        }
-    }
 
-    function control(event){
-        if(event.keyCode == 32){ //a szóköz kódja a 32
-            if(!isJumping){
-                isJumping = true;
-                jump();
-            }
-        }
-    }
+        function mozgatas() {
+            var ember = document.querySelectorAll(".item");
 
-    function generateSalata(){
-        let randomTime = (Math.random()*2000)+500;
-        let salataPosition = 1500;
-        let salata = document.createElement('div');
+            ember.forEach(function (item) {
+                var balPozicio = parseInt(item.style.left);
+                item.style.left = balPozicio - 5 + "px";
 
-        if(!isGameOver){
-            salata.classList.add('salata');
-            salata.style.left=salataPosition + 'px';
-            grid.appendChild(salata);
-        }
-        let timer = setInterval(() => {
-            if(salataPosition > 0 && salataPosition < 60 && position < 60){
-                clearInterval(timer);
-                alert.innerHTML  = 'Game Over! Score: ' + score;
-                isGameOver = true;
-                while(grid.firstChild){
-                    grid.removeChild(grid.lastChild);
+                if (
+                    balPozicio < jatekos.offsetLeft + jatekos.offsetWidth &&
+                    balPozicio + item.offsetWidth > jatekos.offsetLeft &&
+                    item.offsetTop < jatekos.offsetTop + jatekos.offsetHeight &&
+                    item.offsetHeight + item.offsetTop > jatekos.offsetTop
+                ) {
+                    if (item.classList.contains("repa")) {
+                        pontszam++;
+                        item.remove();
+                        ujPontszam();
+                    } else if (item.classList.contains("salata")) {
+                        vegetErtJatek();
+                    }
                 }
-            }
-            if(salataPosition == 0){
-                score++;
-                alert.innerHTML = score;
-            }
-            salataPosition -= 10;
-            salata.style.left = salataPosition + 'px';
-        }, 30)
 
-        if(!isGameOver){
-            setTimeout(generateSalata, randomTime);
+                if (balPozicio < 0) {
+                    item.remove();
+                }
+            });
+
+            if (!jatekVege) {
+                requestAnimationFrame(mozgatas);
+            }
         }
-    }
 
-    document.addEventListener('keyup', control);
-    generateSalata();
+        function ujPontszam() {
+            document.getElementById("pontszam").innerText = "Pontszám: " + pontszam;
+        }
 
-})
+        function vegetErtJatek() {
+            jatekVege = true;
+            alert("Játék vége! Maximális pontszám: " + pontszam);
+            ujJatek();
+        }
+
+        function ujJatek() {
+            jatekVege = false;
+            pontszam = 0;
+            ujPontszam();
+        }
+
+        document.addEventListener("keydown", function (event) {
+            if (event.key === "ArrowUp" && jatekos.offsetTop > 0) {
+                jatekos.style.top = jatekos.offsetTop - 10 + "px";
+            } else if (event.key === "ArrowDown" && jatekos.offsetTop < jatekPalya.clientHeight - jatekos.offsetHeight) {
+                jatekos.style.top = jatekos.offsetTop + 10 + "px";
+            }
+        });
+
+        setInterval(function () {
+            kepGeneral("repa");
+        }, 1500);
+
+        setInterval(function () {
+            kepGeneral("salata");
+        }, 800);
+
+        mozgatas();
